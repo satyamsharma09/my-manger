48ec99f0a3ef7311245abdfa48520437d0badea2
satyamsharma09 <satyamsharma0914@gmail.com>
2025-04-15 21:28:53 +0530

    modify components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <Button size="lg">Click Me</Button>
-      <p className="text-2xl text-green-500 font-bold">
-      Antonio
-    </p>
+    <div className="">
+      <Input />
+      <Input type="email" />
+      <Input type="password" />
+      <Button size="lg">Primary</Button>
+      <Button variant="secondary" size="lg">Secondary</Button>
+      <Button variant="ghost" size="lg">Ghost</Button>
+      <Button variant="muted" size="lg">Muted</Button>
+      <Button variant="destructive" size="lg">Destructive</Button>
+      <Button variant="outline" size="lg">Outline</Button>
+      <Button variant="default" size="lg">Default</Button>
+      <Button variant="teritary" size="lg">Link</Button>
     </div>
 
   );
